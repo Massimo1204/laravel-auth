dfd20a5a5b71cfd8b06c9f6a8f39978bee458a69
Massimo1204 <xiangmassimo@gmail.com>
2022-05-25 00:45:51 +0200

    post user table seeder model

---
+++ app/Models/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\User;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'user_id', 'title', 'content', 'image_source', 'slug'
+    ];
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guests.home');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function () {
+        Route::resource('/', 'HomeController');
+});
